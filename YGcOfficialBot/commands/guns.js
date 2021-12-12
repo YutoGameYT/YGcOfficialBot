bdb6e3704b19c07fff37bbdd26fe2dcd3fece099
YutoGameYT <95849296+YutoGameYT@users.noreply.github.com>
2021-12-12 10:53:23 +0900

    Delete YGcOfficialBot directory

--- YGcOfficialBot/commands/guns.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "guns",
-    description: "You Can GACHAGACHA xD",
-    execute(message, args, client, Discord) {
-        const array = ["銃が出てきた\n\nポンプショットガン","YGガチャ\n\nアサルトライフル！:gun:","銃が出てきた\n\nサブマシンガン","銃が出てきた\n\nスナイパーライフル","銃が出てきた\n\nロケラン:boom:"];
-            message.reply(array[Math.floor(Math.random() * array.length)]);
-            console.log(message.guild.name + "\n" + message.author.username + "\n" + array[Math.floor(Math.random() * array.length)]);
-    }
-}
--- YGcOfficialBot/commands/website.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'website',
-    description: "this is a website command!",
-    execute(message, args, client, Discord){
-        message.reply('https://youtube.com/ygcyt')
-    }
-}
